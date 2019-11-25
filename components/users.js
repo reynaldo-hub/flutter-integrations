e1b21e8279594e2b0bd6ead41b7a6d3332dc3501
Reynaldo Rodriguez <rodriguezreynaldo1698@outlook.com>
2019-11-24 20:39:38 -0600

    Use async await instead of promises on user component

--- components/users.js
+++ components/users.js
@@ -2,17 +2,18 @@ const axios = require('axios');
 const config = require('../config.json');
 const Person = require('../models/user');
 
-const APIIURL = `${config.RandomUserUrl}api/`;
+const APIIURL = `${config.RandomUserUrl}`;
 
-const getRandomUser = (request, response) => axios.get(APIIURL)
-  .then((resp) => {
-    const { data: { results: [{ name: { first: firstName, last: lastName }, picture: { large: picture } }] } } = resp;
+const getRandomUser = async (request, response) => {
+  try {
+    const res = await axios.get(APIIURL);
+
+    const { data: { results: [{ name: { first: firstName, last: lastName }, picture: { large: picture } }] } } = res;
     const person = new Person(firstName, lastName, picture);
-
     response.status(200).json(person);
-  })
-  .catch((error) => {
+  } catch (error) {
     response.status(500).send(error);
-  });
+  }
+};
 
 module.exports = getRandomUser;
